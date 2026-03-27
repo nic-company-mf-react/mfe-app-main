2d923d5ff3eeaba9b2536ff807a9dba4ca9c10bc
redsky0212 <wihyun0238@gmail.com>
2026-03-27 15:49:23 +0900

    $router 객체 등록 코드 삽입

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -1,12 +1,14 @@
 import { createAppRouter } from './app-common-router.ts';
 import routes from '@/shared/router';
+import { registerWindowRouter } from '@nic/mfe-lib-shared/utils';
 
 const router = createAppRouter(routes, {
 	// .env 파일에 설정된 VITE_ROUTER_BASENAME 값을 사용합니다.
 	basename: import.meta.env.VITE_ROUTER_BASENAME,
 });
 
-//Router.getInstance().setRouterInstance(router);
+// host 앱이 window.$router를 등록 (모든 remote 앱이 공유)
+registerWindowRouter(router);
 
 export * from './app-common-router.ts';
 export default router;
